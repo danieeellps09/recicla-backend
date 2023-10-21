9fc0f64dc9ba7e12a49614a634b36904ee4e20c7
daniel lopes <daniel-lps@hotmail.com.br~>
2023-10-21 11:30:46 -0300

    ajeitando a logica de validar cnpj

---
+++ src/associacoes/decorators/cnpj.decorator.ts
@@ -0,0 +1,62 @@
+// cnpj.decorator.ts
+import {
+    registerDecorator,
+    ValidationArguments,
+    ValidationDecoratorOptions,
+    ValidatorConstraint,
+    ValidatorConstraintInterface,
+    ValidationOptions,
+} from 'class-validator';
+
+@ValidatorConstraint({ name: 'isCnpjValid', async: false })
+export class IsCnpjValidConstraint implements ValidatorConstraintInterface {
+    validate(cnpj: string, args: ValidationArguments) {
+        // Remover caracteres não numéricos
+        const cleanedCnpj = cnpj.replace(/\D/g, '');
+
+        // Validar se o CNPJ possui 14 dígitos após remover não numéricos
+        if (cleanedCnpj.length !== 14) {
+            return false;
+        }
+
+        // Lógica de validação dos dígitos
+        const calcCnpj = cleanedCnpj.substring(0, 12);
+        const digit1 = this.calculateDigit(calcCnpj, 5);
+        const digit2 = this.calculateDigit(calcCnpj + digit1, 6);
+
+        return (
+            cleanedCnpj === calcCnpj + digit1.toString() + digit2.toString() ||
+            cnpj ===
+                `${cleanedCnpj.substring(0, 2)}.${cleanedCnpj.substring(
+                    2,
+                    5
+                )}.${cleanedCnpj.substring(5, 8)}/${cleanedCnpj.substring(
+                    8,
+                    12
+                )}-${cleanedCnpj.substring(12)}`
+        );
+    }
+
+    private calculateDigit(cnpj: string, base: number): number {
+        const sum = cnpj.split('').reduce((acc, value) => {
+            const digit = parseInt(value, 10);
+            return acc + digit * base--;
+        }, 0);
+
+        const remainder = sum % 11;
+        return remainder < 2 ? 0 : 11 - remainder;
+    }
+}
+
+export function IsCnpjValid(validationOptions?: ValidationOptions) {
+    return function (object: Record<string, any>, propertyName: string) {
+        registerDecorator({
+            target: object.constructor,
+            propertyName: propertyName,
+            options: validationOptions,
+            constraints: [],
+            validator: IsCnpjValidConstraint,
+        });
+    };
+}
+
--- src/associacoes/dto/update-associacao.dto.ts
+++ src/associacoes/dto/update-associacao.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsNumber, IsString } from "class-validator";
-import { IsCnpjValid } from "src/decorators/cnpj.decorator";
+import { IsCnpjValid } from "src/associacoes/decorators/cnpj.decorator";
 
 export class UpdateAssociacaoDto{
 
